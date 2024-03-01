34c92531a9638146b72472c2b7d54cae2c771ba8
giamaf <gianluca.maffucci@hotmail.com>
2024-03-01 16:01:56 +0100

    query_select_solution_3

--- query_select_solutions.sql
+++ query_select_solutions.sql
@@ -9,6 +9,10 @@ FROM `courses`
 WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT `name` AS `nome`, `surname` AS `cognome`, `date_of_birth` AS `data di nascita`
+FROM `students`
+WHERE YEAR(`date_of_birth`) < 1994;
+
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
