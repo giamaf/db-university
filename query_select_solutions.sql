7315611535bd313a76c90182b22e1606b841a2a3
giamaf <gianluca.maffucci@hotmail.com>
2024-03-01 16:38:25 +0100

    query_select_solution_8

--- query_select_solutions.sql
+++ query_select_solutions.sql
@@ -33,3 +33,6 @@ SELECT COUNT(`id`) as `numero_dipartimenti`
 FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT *
+FROM `teachers`
+WHERE `phone` IS NULL;
