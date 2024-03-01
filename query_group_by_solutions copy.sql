6a274935e043028a13691606e83ea8df04912922
giamaf <gianluca.maffucci@hotmail.com>
2024-03-01 16:56:46 +0100

    query_group_by_solution_3

--- query_group_by_solutions copy.sql
+++ query_group_by_solutions copy.sql
@@ -9,4 +9,8 @@ FROM `teachers`
 GROUP BY `office_number`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`), 2) AS `avg_vote`, `exam_id` AS `exam_attempt`
+FROM `exam_student`
+GROUP BY `exam_attempt`;
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
