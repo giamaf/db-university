478760741ab009be22496d6a74159453515c5b10
giamaf <gianluca.maffucci@hotmail.com>
2024-03-04 18:38:59 +0100

    query_join_solution_6

--- query_join.sql
+++ query_join.sql
@@ -55,4 +55,15 @@ JOIN `course_teacher` AS CT ON CT.`course_id` = C.`id`
 JOIN `teachers` AS T ON CT.`teacher_id` = T.`id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT 
+    DISTINCT T.`name` AS 'Nome Docente',
+             T.`surname` AS 'Cognome Docente',
+             DEP.`name` AS 'Nome Dipartimento' 
+FROM `teachers` AS T 
+JOIN `course_teacher` AS CT ON CT.`teacher_id` = T.`id` 
+JOIN `courses` AS C ON CT.`course_id` = C.`id` 
+JOIN `degrees` AS DEG ON C.`degree_id` = DEG.`id` 
+JOIN `departments` AS DEP ON DEG.`department_id` = DEP.`id` 
+WHERE DEP.`name` = 'Dipartimento di Matematica';
+
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
